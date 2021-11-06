63d7d5b5a49898dc532c4414bfbafedcadbb43a5
MarcoTorr <marco.torres@aiesec.net>
2021-11-06 14:18:12 -0600

    Changed server.js port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express= require("express");
 const path= require ("path");
 const app = express();
-const PORT = process.env.port || 3003;
+const PORT = process.env.PORT || 3003;
 const api = require('./routes/index.js');
 
 
